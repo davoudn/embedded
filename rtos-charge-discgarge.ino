105c1cad0219fa8cdce5a461880103886270b500
davoudn <dd.nasr@gmail.com>
2022-12-16 20:55:27 +0330

    calss reconstruction

--- rtos-charge-discgarge.ino
+++ rtos-charge-discgarge.ino
@@ -9,15 +9,35 @@
 // It will be used to ensure only one Task is accessing this resource at any time.
 SemaphoreHandle_t xSerialSemaphore;
 // Logical control variables
-bool taskStarted = false;
-bool taskFinished = false;
+//bool taskStarted = false;
+//bool taskFinished = false;
 bool command_arrived= false;
 TaskHandle_t taskMeassureHandle;
 TaskHandle_t taskRunHandle;
 
+// define two Tasks for DigitalRead & AnalogRead
+void taskControl( void *pvParameters );
+void write_to_dac(int dac_id, float value);
+void cellOff();
+void taskListen( void *pvParameters );
+
+void write_to_dac(int dac_id, float value){
+  // transmit to device #4
+  int dummy = (value/10.0)*4095;
+  byte myBytes[2];
+  myBytes[0] = dummy/256;
+  myBytes[1] = dummy%256; 
+  Wire.beginTransmission(96);
+  Wire.write(myBytes[0]);  
+  Wire.write(myBytes[1]);            // sends instruction byte  
+ // Wire.write(x2);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+    return;
+}
+
 // JSON variables
 DeserializationError error;
-StaticJsonDocument<120>  json_instruction, json_data, json_message, json_ins_arrived;
+StaticJsonDocument<120>  json_instruction, json_data, json_ins_arrived;
 String json_string_instruction = "", str = ""; //read until timeout
 
 struct Sample{
@@ -26,128 +46,162 @@ struct Sample{
   float m_time_interval;
 };
 
-Sample sample;
-// define two Tasks for DigitalRead & AnalogRead
-void taskChargeDischarge( void *pvParameters );
-void taskChargeDischarge( void *pvParameters );
-void taskControl( void *pvParameters );
-void removeTasks( void *pvParameters );
-void write_to_dac(int dac_id, float value);
-void cellOff();
+struct Flags{
+  bool m_taskStarted;
+  bool m_taskFinished;
+}
+
 //
-struct cv{
-};
-
-struct  cc{
-  static void finished(bool& para){
-                  digitalWrite(50,LOW); 
-                  para = true;
-                  cellOff();  
-                  }  
-
-};
-
+struct cv{};
+struct cc{};
 template <class T>
-struct check;
+struct procedure;
 
-template <> struct check<cv>{
+
+template <typename PROCTYPE>
+sctruct  base {
+  private:
+    PROCTYPE proc;
+
+  public:
+
+  base(StaticJsonDocument<120>& j_instruction_):proc(j_instruction_){}
+  ~base(){}
   
-  static void ckeck_it(Sample& s, StaticJsonDocument<120>& jpara, bool& para, void (*action)(bool&)){ 
-    float dummy = jpara["field_cutoff"];
-    if (s.m_current * s.m_voltage < dummy * s.m_voltage) action(para);
-    return;    
-    }
-  static void finished(bool& para){
-                  digitalWrite(50,LOW); 
-                  para = true;
-                  cellOff();  
-                  return;
-                  } 
-  static void apply (StaticJsonDocument<120>& json_instruction_){
-          pinMode(22, OUTPUT);
-          digitalWrite(22, HIGH);     
-          float applied_voltage = json_instruction_["applied_field"];
-          write_to_dac(96, applied_voltage);
-          pinMode(50, OUTPUT);
-          digitalWrite(50, HIGH);      
-          return;
+  void run(){
+         xTaskCreate( base<PROCTYPE>::taskMeassure, "Meassureing"   ,  256,  &proc->m_sample, 3,   &taskMeassureHandle ); //Task Handle
+         xTaskCreate( base<PROCTYPE>::taskCheck   , "Runing the job",  256,  &proc->m_flags , 3,   &taskRunHandle );
   }
-  static void run(StaticJsonDocument<120>& json_arrived_){
-         xTaskCreate( taskMeassure, "Meassureing"   ,  256,  NULL, 3,   &taskMeassureHandle ); //Task Handle
-         xTaskCreate( check<cv>::taskRun     , "Runing the job",  256,  NULL,  3,   &taskRunHandle );
-	 }
-  static void taskRun(StaticJsonDocument<120>& json_arrived_){
-  	check<cv>::apply(json_instruction);
-        json_message["msg"]="jobstarted";
-	json_message["type"]="message";
-	serializeJson(json_message, Serial); 
-  	for (;;){
-          if (taskStarted && !taskFinished){
-                                        check<cv>::do_it(sample,json_instruction,taskFinished,&check<cv>::finished);
-                                        if (!taskFinished) 
-                                               sendData(json_data);
-                                        else                
-                                               sendMessageFinished();
-          }   
+  
+  void ckeck_it(){ 
+          proc->check_it();
+          return;    
+  }
+
+  void apply (){
+        proc->apply();  
+        return;
+  }
+ 
+  void sendData(){
+       proc->sendData();
+  }
+
+  void sendMessageFinished(){
+   //       if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 1 ) == pdTRUE ){
+       proc->sendMessageFinished();
+                                                           //                         xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others. 
+  }
+
+  void removeTasks(){
+       proc->json_data["current"] = 0.0;
+       proc->json_data["voltage"] = 0.0;
+       proc->m_sample.m_voltage=0.0;
+       proc->m_sample.m_current=0.0;
+       proc->m_sample.m_time_interval=0.0;
+       // vTaskDelete(taskRunHandle);
+  }
+
+  /* ----------------------------      Static functions      --------------------------------- */
+
+   static void taskCheck(PROCTYPE* prc){
+        for (;;){
+          if (prc->taskStarted && !prc->taskFinished){
+             prc->check_it();
+             if (!prc->taskFinished)
+                prc->sendData();
+             else
+                prc->sendMessageFinished();
+          }
        }
   }
 
-void removeTasks(void *any)
-{
-  json_data["current"] = 0.0;
-  json_data["voltage"] = 0.0;
-  sample.m_voltage=0.0;
-  sample.m_current=0.0;
-  sample.m_time_interval=0.0;
- // vTaskDelete(taskMeassureHandle);
- // vTaskDelete(taskRunHandle);
-       
+  static void taskMeassure(PROCTYPE *prc){
+     float averageValue = 500.0;
+     int meassure_interval = 2000;
+     long int sensorValue = 0;  // variable to store the sensor value read
+     long int sensorValue1 = 0;
 
+     float current_voltage = 0.0;
+     float voltage_voltage=0.0;
+     float time = 0;
+
+     for (;;) {
+  //  if (time >= meassure_interval ){
+       sensorValue = 0;
+       sensorValue1 = 0;
+       for (int i = 0; i < averageValue; i++) {
+           sensorValue += analogRead(A0);
+           sensorValue1+= analogRead(A1);
+           delay(0.5);
+       }
+       sensorValue = sensorValue / averageValue;
+       current_voltage = sensorValue * 5.0 / 1024.0;
+       prc->sample.m_current = (current_voltage - 2.5) / 0.066;
+       prc->sample.m_voltage = 5.0 * sensorValue1 / averageValue / 1024.0; //
+       prc->sample.m_time_interval = time*0.001; // in seconds!!
+   // }
+     }
   }
+
 };
 
-template <> struct check<cc>{
-  static void check_it(Sample& s, StaticJsonDocument<120>& jpara, bool& para, void (*action)(bool&)){ 
-    float dummy = jpara["field_cutoff"];
-    if (s.m_voltage * s.m_current > dummy * s.m_current) action(para);
+
+template <> struct procedure<cv> {
+   StaticJsonDocument<120> j_instruction, j_data, j_message;
+   Sample m_sample;
+   Flags  m_flags;
+  procedure (StaticJsonDocument<120> j_instruction_):j_instruction(j_instruction_) {}
+  ~procedure(){}
+
+  void ckeck_it(){ 
+          float dummy = j_instruction["field_cutoff"];
+          if (m_sample.m_current * m_sample.m_voltage < dummy * m_sample.m_voltage) done();
+          return;    
+  }
+
+  void done(){
+    m_flags.m_taskFinished = true;
+    digitalWrite(50,LOW);
+    cellOff();
+    return;
+  }
+
+  void apply (){
+         json_message["msg"]="jobstarted";
+         json_message["type"]="message";
+         serializeJson(json_message, Serial);
+         pinMode(22, OUTPUT);
+         digitalWrite(22, HIGH);     
+         float applied_voltage = json_instruction_["applied_field"];
+         write_to_dac(96, applied_voltage);
+         pinMode(50, OUTPUT);
+         digitalWrite(50, HIGH);      
+        return;
+   }
+
+   void sendData(){
+         json_data["type"] = "data";
+         json_data["current"] = sample.m_current;
+         json_data["voltage"] = sample.m_voltage;
+         json_data["time_interval"] = sample.m_time_interval;
+         serializeJson(json_data, Serial); 
+         Serial.print("#\n");    
     }
-    
-  static void finished(bool& para){
-                  digitalWrite(50,LOW); 
-                  para = true;
-                  cellOff();  
-                  return;
-                  } 
-                  
-  static void apply (StaticJsonDocument<120>& json_instruction_){
-                                                     pinMode(22, OUTPUT);
-                                                     digitalWrite(22,LOW);   //set pin 8 HIG*/   
-                                                     float applied_current = json_instruction["applied_field"];
-                                                     write_to_dac(96, applied_current);
-                                                     pinMode(50, OUTPUT);
-                                                     digitalWrite(50,HIGH);   //set pin 8 HIG*/     
-          return;
-  }
-};
 
-void sendData(StaticJsonDocument<120>& json_data_){
-    json_data_["type"] = "data";
-    json_data_["current"] = sample.m_current;
-    json_data_["voltage"] = sample.m_voltage;
-    json_data_["time_interval"] = sample.m_time_interval;
-    serializeJson(json_data_, Serial); 
-    Serial.print("#\n");    
-}
-
-void sendMessageFinished(){
-                                                       //       if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 1 ) == pdTRUE ){
-                                                                                    json_message["type"] = "message"  ;     
-                                                                                    json_message["msg"] = "jobdone"   ;  
-                                                                                    serializeJson(json_message, Serial);    
-                                                                                    Serial.print("#\n");    
+    void sendMessageFinished(){
+     //       if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 1 ) == pdTRUE ){
+         json_message["type"] = "message"  ;     
+         json_message["msg"] = "jobdone"   ;  
+         serializeJson(json_message, Serial);    
+         Serial.print("#\n");    
                                                            //                         xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
                                                            //   }            
-}
+    }
+  };
+
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
 
@@ -201,9 +255,9 @@ void taskControl( void *pvParameters )  // This is a Task.
     // Now set up two Tasks to run independently.
      if ( command_arrived && !taskStarted && !taskFinished ){  
         if (json_ins_arrived["command2"] == "cv"){
+            Base < Procedure<cv> > base_proc_cv (json_ins_arrived).run();
         //xTaskCreate( taskMeassure, "Meassureing"   ,  256,  NULL, 3,   &taskMeassureHandle ); //Task Handle
         //xTaskCreate( taskRun     , "Runing the job",  256,  NULL,  3,   &taskRunHandle     ); //Task Handle    
-	  check<cv>::run(json_ins_arrived);
 	}
         taskStarted = true; 
         command_arrived = false;          
@@ -215,7 +269,7 @@ void taskControl( void *pvParameters )  // This is a Task.
             else {Serial.print("A task is running, first cancel the current task!!!");}
      }// Now free or "Give" the Serial         
      else if( taskStarted && taskFinished ) {
-          removeTasks(NULL);
+          //removeTasks(NULL);
          // removeTasks(taskRunHandle);
        }// Now free or "Give" the Serial                        
      } 
@@ -236,58 +290,9 @@ void taskListen( void *pvParameters )  // This is a Task.
     //  xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.;
   }
 }
-/* ----------------------------------------------------------------------------------------- */
-void taskMeassure( void *pvParameters)  // This is a Task.
-{
-    // make the pushbutton's pin an input:
-// Sensor reading aux variables
-const float averageValue = 500.0;
-const int meassure_interval = 2000;
-long int sensorValue = 0;  // variable to store the sensor value read
-long int sensorValue1 = 0;
-
-float current_voltage = 0.0;
-float voltage_voltage=0.0;
-float time = 0;    
-
-  for (;;) {
-    // read the input pin:
-  //  if (time >= meassure_interval ){  
-       sensorValue = 0;
-       sensorValue1 = 0;
-    for (int i = 0; i < averageValue; i++) {
-       sensorValue += analogRead(A0);
-       sensorValue1+= analogRead(A1);
-    // wait 2 milliseconds before the next loop
-       delay(0.5);
-    }
-    sensorValue = sensorValue / averageValue;
-    current_voltage = sensorValue * 5.0 / 1024.0;
-    sample.m_current = (current_voltage - 2.5) / 0.066;  
-    sample.m_voltage = 5.0 * sensorValue1 / averageValue / 1024.0; // 
-    sample.m_time_interval = time*0.001; // in seconds!!
-   // }
-  }
-}
-
 
 void cellOff()
 {
   return;  
 }
-
-void write_to_dac(int dac_id, float value){
-  // transmit to device #4
-  int dummy = (value/10.0)*4095;
-  byte myBytes[2];
-  myBytes[0] = dummy/256;
-  myBytes[1] = dummy%256; 
-  Wire.beginTransmission(96);
-  Wire.write(myBytes[0]);  
-  Wire.write(myBytes[1]);            // sends instruction byte  
- // Wire.write(x2);              // sends one byte  
-  Wire.endTransmission();    // stop transmitting
-    return;
-}
-
 
